f165d681b4ce1d8189aa4cf7aa5cda4fea02e755
Mauro Rojas <maurorojascabj@gmail.com>
2021-06-29 13:58:17 -0300

    Added the login component with his unit test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from 'react';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App w-100 bg-light">
+      <Login/>
     </div>
   );
 }
---
+++ src/test/login.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import {Login} from '../components/Login';
+
+describe('Comprobar login', () => {
+    it('Podría existir un submit button', () => {
+        render( <Login/>)
+        
+        expect(
+            screen.getByRole('button',{ name: /Submit/i}),
+            ).toBeInTheDocument();
+        })
+})
