f60dfcc41fedc2677dceec6b3b3d3bc666010521
Mauro Rojas <maurorojascabj@gmail.com>
2021-06-30 19:47:14 -0300

    Modified the Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-const sumar = () => {
-    return a + b;
-}
-
-
 export const Login = () => {
     return (
     <>
@@ -15,7 +10,7 @@ export const Login = () => {
                     <div className="row justify-content-center">
                         <div className="col-md-10">
                             <p>User</p>
-                            <input type="text" name="user" className="form-control"></input>
+                            <input type="text" name="user" className="form-control" onclick="hacerAlgo()"></input>
                         </div>
                     </div>
                     <div className="row justify-content-center my-3">
