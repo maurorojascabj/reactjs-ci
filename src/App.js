f319a7629cdaf6cf6101e9e91dcffcd023c0137e
Mauro Rojas <maurorojascabj@gmail.com>
2021-06-30 20:03:39 -0300

    Deleted one sentence in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Login from './components/Login';
+// import Login from './components/Login';
 
 function App() {
   return (
     <div className="App w-100 bg-light">
       <Login/>
-      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
-        {headingText}
-      </h2>
     </div>
   );
 }
