cbe06e16be2eab8879c1749c096847a036c6141c
Mauro Rojas <maurorojascabj@gmail.com>
2021-07-01 01:16:49 -0300

    New change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from './components/Login';
 function App() {
   return (
     <div className="App w-100 bg-light">
-      <Login/>
+      <Login/> {/* Component Login */}
     </div>
   );
 }
